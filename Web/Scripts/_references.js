031217d4503ed84e553777eb4c48491283c00e9d
Hiromi Ayase <hiromi.ayase@gmail.com>
2014-06-08 01:25:58 +0900

    足りないファイルを追加

---
+++ Web/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="../js/enchant.js" />
+/// <reference path="../js/rot.js" />
+/// <reference path="../js/common.js" />
+/// <reference path="../js/units.js" />
+/// <reference path="../js/dungeon.js" />
+
+
---
+++ Web/js/game.js
@@ -0,0 +1,10 @@
+﻿
+
+var game = function () {
+    var dungeon = new Dungeon();
+    dungeon.addUnit(new Ignore());
+    dungeon.engine.start();
+
+    var view = new View() {}
+};
+
